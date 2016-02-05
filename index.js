086092e75cbd1c0f289cfeddd952285156dbd9c1
luke <luke.edwards05@gmail.com>
2016-02-05 15:08:15 -0800

    fully functional xo plugin

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 const xo = require('xo');
+const path = require('path');
 const assign = require('object-assign');
 
 module.exports = function () {
   this.xo = function (opts) {
     opts = assign({quiet: false}, opts);
-    const files = this._.globs;
-    console.log( files, opts );
-    return this;
+    const proj = this.root;
+
+    return xo.lintFiles(this._.globs, opts).then(report => {
+      // truncate project root from path
+      let results = report.results.map(entry => {
+        entry.filePath = path.relative(proj, entry.filePath);
+        return entry;
+      });
+
+      if (opts.quiet) {
+        results = xo.getErrorResults(results);
+      }
+
+      if (report.errorCount > 0 || report.warningCount > 0) {
+        this.error('fly-xo: Code Style Errors \n', xo.getFormatter()(results));
+      }
+    });
   };
-}
+};
