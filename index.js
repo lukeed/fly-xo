6ec2a851bd723978714f438421bb92a9598bcb0e
luke <luke.edwards05@gmail.com>
2016-02-05 14:16:35 -0800

    initial body

--- index.js
+++ index.js
@@ -1,34 +1,11 @@
-export default function (/*debug*/) {
-  this.filter("xo", (data, options) => {
-  /**
-    @overview A filter plugin returns an object { code, map, ext }
-    which is the result of transforming the incomding data source:
+const xo = require('xo');
+const assign = require('object-assign');
 
-      return { code, map, ext }
-
-    @example Sync filter `j` that transforms a given string into an
-    object, i.e, {code, map} where code is the result data and map
-    a sourcemap if `options.sourceMap === true`.
-
-      const j = require("my-js-transformer")
-      const assign = require("object-assign")
-
-      export default function () {
-        return this.filter("j", (data, options) => {
-          return assign({ ext: ".js"}, j.render(data.toString(), options))
-        })
-      }
-
-    @example Async filter `s` that transforms a given string and invokes
-    a callback function with an object, i.e, {css, map}.
-
-    const s = require("my-style-trasformer")
-    const assign = require("object-assign")
-
-    export default function () {
-      return this.defer(s.render)(data.toString(), options).then((result) =>
-        assign({ ext: ".css"}, result))
-    }
-  */
-  })
+module.exports = function () {
+  this.xo = function (opts) {
+    opts = assign({quiet: false}, opts);
+    const files = this._.globs;
+    console.log( files, opts );
+    return this;
+  };
 }
