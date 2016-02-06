56dce0c9ed5c7d4eaf1f5ef58021ee61759c7751
luke <luke.edwards05@gmail.com>
2016-02-05 18:25:27 -0800

    basic test

--- test/test.js
+++ test/test.js
@@ -1,5 +1,10 @@
-const test = require("tape").test
+const xo = require('../');
+const test = require('tape').test;
+const join = require('path').join;
 
-test("fly-xo", (t) => {
-  t.end()
-})
+test('fly-xo', function (t) {
+  const fly = {};
+  xo.call(fly);
+  t.ok(fly.xo !== undefined, 'add xo function to fly instance');
+  t.end();
+});
