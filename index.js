d3e33fda03692d57ef03bee092b5df681bab44be
Luke Edwards <luke.edwards05@gmail.com>
2016-10-17 10:05:30 +0200

    upgrade plugin logic

--- index.js
+++ index.js
@@ -1,37 +1,37 @@
+'use strict';
+
+const rel = require('path').relative;
 const xo = require('xo');
-const path = require('path');
-const assign = require('object-assign');
 
 module.exports = function () {
-  this.xo = function (opts) {
-    opts = assign({quiet: false}, opts);
-    const proj = this.root;
+	this.plugin('xo', {every: 0, files: 0}, function * (globs, opts) {
+		opts = Object.assign({quiet: false}, opts);
 
-    return xo.lintFiles(this._.globs, opts).then(report => {
-      // truncate project root from path
-      var results = report.results.map(entry => {
-        entry.filePath = path.relative(proj, entry.filePath);
-        return entry;
-      });
+		const report = yield xo.lintFiles(globs, opts);
 
-      // hide warnings
-      if (opts.quiet) {
-        results = xo.getErrorResults(results);
-      }
+		// truncate project root from paths
+		let results = report.results.map(obj => {
+			obj.filePath = rel(this.root, obj.filePath);
+			return obj;
+		});
 
-      if (report.errorCount > 0 || report.warningCount > 0) {
-        this.log(xo.getFormatter()(results));
-      }
+		// hide warnings
+		if (opts.quiet) {
+			results = xo.getErrorResults(results);
+		}
 
-      if (report.errorCount > 0) {
-        const num = results.filter(el => el.errorCount > 0).length;
-        const end = (num > 1) ? 'files!' : 'file!';
+		if (report.errorCount > 0 || report.warningCount > 0) {
+			this.$.log(xo.getFormatter()(results));
+		}
 
-        this.emit('plugin_error', {
-          plugin: 'fly-xo',
-          error: 'XO found '+ report.errorCount +' errors in '+ num +' '+end
-        });
-      }
-    });
-  };
+		if (report.errorCount > 0) {
+			const num = results.filter(el => el.errorCount > 0).length;
+			const end = (num > 1) ? 'files' : 'file';
+
+			this.emit('plugin_error', {
+				plugin: 'fly-xo',
+				error: `XO found ${report.errorCount} errors in ${num} ${end}!`
+			});
+		}
+	});
 };
